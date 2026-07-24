f517a4d280a1bef6360521d3b2e9e7e9ee90380f
NulLundVoidd <taylorlolmo@gmail.com>
2026-07-23 20:32:23 -0700

    Expose test level navigation paths

--- gmtk-26/scripts/test_level.gd
+++ gmtk-26/scripts/test_level.gd
@@ -7,6 +7,9 @@ const MUSIC_REGION_SCENE := preload("res://scenes/area_music_region.tscn")
 @export_category("Test Level Flow")
 @export var goal_position := Vector3(-14.0, 8.0, -112.0)
 @export var area_music_position := Vector3(-8.0, 2.0, -45.0)
+@export_file("*.tscn") var next_level_path: String
+@export_file("*.tscn") var main_menu_path: String
+@export_range(0.0, 10.0, 0.1) var completion_delay := 0.75
 
 
 func _enter_tree() -> void:
@@ -14,6 +17,9 @@ func _enter_tree() -> void:
 	level_goal.name = "LevelGoal"
 	level_goal.unique_name_in_owner = true
 	level_goal.position = goal_position
+	level_goal.next_level_path = next_level_path
+	level_goal.main_menu_path = main_menu_path
+	level_goal.completion_delay = completion_delay
 	add_child(level_goal)
 
 	var screen := RESULT_SCREEN_SCENE.instantiate() as LevelResultScreen
