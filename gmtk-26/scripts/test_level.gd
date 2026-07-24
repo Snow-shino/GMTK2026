dab7866ecf5710dc5eaae7d05ea5e67d1531d3ab
NulLundVoidd <taylorlolmo@gmail.com>
2026-07-23 20:26:39 -0700

    Add level restart and collectible respawns

--- gmtk-26/scripts/test_level.gd
+++ gmtk-26/scripts/test_level.gd
@@ -9,5 +9,10 @@ func _ready() -> void:
 	player.life_depleted.connect(_on_player_life_depleted)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().call_deferred("reload_current_scene")
+
+
 func _on_player_life_depleted() -> void:
 	print("Life Left reached zero.")
